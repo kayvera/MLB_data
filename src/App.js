3ba33465795b866638e8d7288e87d0f7e9ce6aa9
kayvera <mikaylarvr@gmail.com>
2020-07-25 23:16:58 -0700

    redid npm install to update modules and added drawer component files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from "react";
-import Header from "./components/Header";
-import Menu from "./components/Menu";
-import Content from "./components/Content";
-import Footer from "./components/Footer";
 
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <Header />
-        <Menu />
-        <Content />
-        <Footer />
-      </div>
-    );
+    return <div></div>;
   }
 }
 
